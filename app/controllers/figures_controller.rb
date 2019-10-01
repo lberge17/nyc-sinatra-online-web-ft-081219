b81d469edf260b9c9ff58df0f7269339af768696
Laura Berge <lauraeberge@gmail.com>
2019-10-01 18:25:02 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,25 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    "Here should be all the figures"
+  end
+  
+  get '/figures/new' do
+    "Here should be a form to create new figures"
+  end
+  
+  post '/figures' do
+    "Here we save the new figure and redirect to that figure's page"
+  end
+  
+  get '/figures/:id' do
+    "Here is an individual figure"
+  end
+  
+  get '/figures/:id/edit' do
+    "Here you can edit an individual figure"
+  end
+  
+  patch '/figures' do
+    "Here we save the updated figure and redirect to that figure's page"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,25 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    "Here should be all the landmarks"
+  end
+  
+  get '/landmarks/new' do
+    "Here should be a form to create new landmarks"
+  end
+  
+  post '/landmarks' do
+    "Here we save the new landmark and redirect to that landmark's page"
+  end
+  
+  get '/landmarks/:id' do
+    "Here is an individual landmark"
+  end
+  
+  get '/landmarks/:id/edit' do
+    "Here you can edit an individual landmark"
+  end
+  
+  patch '/landmarks' do
+    "Here we save the updated landmark and redirect to that landmark's page"
+  end
 end
